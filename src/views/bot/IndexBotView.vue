59fb1727737209fb34801e158212cde78a042e0f
eduardocoello16 <eduardocoello16@gmail.com>
2023-08-29 23:59:41 +0100

    preparado

--- src/views/bot/IndexBotView.vue
+++ src/views/bot/IndexBotView.vue
@@ -1,14 +1,28 @@
 <template>
     <h2>Prueba</h2>
+   <p>Hola  {{ nombre }}</p>
+   <p>Instancia {{ instance }}</p>
 </template>
 <script lang="ts">
  import { loadScript } from "vue-plugin-load-script";
 export default {
- async mounted() {
-   await loadScript("https://telegram.org/js/telegram-web-app.js")
-   console.log(window.Telegram)
- 
+ async created() {
+  await loadScript("http://telegram.org/js/telegram-web-app.js")
+if(window.Telegram.WebApp.initDataUnsafe.user){
+  this.nombre =  window.Telegram.WebApp.initDataUnsafe.user?.first_name
+  this.instance = window.Telegram.WebApp.initDataUnsafe.chat_instance
+}
 
+
+
+
+
+  },
+  data() {
+    return {
+      nombre: '',
+      instance: ''
+    }
   }
 };
 </script>
