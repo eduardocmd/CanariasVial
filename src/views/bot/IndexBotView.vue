23c4dfd709d06ecf78e9249cd320d93a9e3cfbe0
eduardocoello16 <eduardocoello16@gmail.com>
2023-08-31 00:28:05 +0100

    init data

--- src/views/bot/IndexBotView.vue
+++ src/views/bot/IndexBotView.vue
@@ -8,6 +8,7 @@
 export default {
  async created() {
   await loadScript(import.meta.env.VITE_APP_Telegram_Script)
+  console.log(window.Telegram.WebApp.initDataUnsafe)
 if(window.Telegram.WebApp.initDataUnsafe.user){
   this.nombre =  window.Telegram.WebApp.initDataUnsafe.user?.first_name
  
