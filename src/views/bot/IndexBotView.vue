8a11a7561e4176602d4d2c6416462f37a3fbdc8b
eduardocoello16 <eduardocoello16@gmail.com>
2023-08-31 16:05:13 +0100

    creando ref

--- src/views/bot/IndexBotView.vue
+++ src/views/bot/IndexBotView.vue
@@ -1,15 +1,20 @@
 <template>
     <h2>Prueba</h2>
+    {{ nombre }}
 <AlertsSelector/>
 </template>
 <script setup lang="ts">
   import { loadScript } from "vue-plugin-load-script";
 import AlertsSelector from "@/components/bot/AlertsSelector.vue"
-import { onMounted } from "vue"
+import { ref, onMounted } from "vue"
+const nombre = ref('')
 
 onMounted(async() => {
-
   await loadScript(import.meta.env.VITE_APP_Telegram_Script);
+if(window.Telegram.WebApp.initDataUnsafe.user?.first_name){
+  nombre.value =window.Telegram.WebApp.initDataUnsafe.user?.first_name
+}
+  
   
 
 })
