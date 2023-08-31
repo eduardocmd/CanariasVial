caadd464d284bf0405bf3bb22b9cac81bd8c3d13
eduardocoello16 <eduardocoello16@gmail.com>
2023-09-01 00:28:48 +0100

    status

--- src/views/bot/IndexBotView.vue
+++ src/views/bot/IndexBotView.vue
@@ -1,23 +1,53 @@
 <template>
-  <h2>Hola   {{ nombre }}</h2>
+  <main>
+    <section v-if="status">
+      <h2>Prueba</h2>
+      {{ nombre }}
+      <AlertsSelector />
+    </section>
+    <section v-else>
+<p>No cargó</p>
+    </section>
 
-  <AlertsSelector />
+  </main>
 </template>
 <script setup lang="ts">
 import { loadScript } from "vue-plugin-load-script";
 import AlertsSelector from "@/components/bot/AlertsSelector.vue"
 import { ref, onMounted } from "vue"
+import axios from 'axios';
+
 const nombre = ref('')
+let status = ref(false)
 
 onMounted(async () => {
   await loadScript(import.meta.env.VITE_APP_Telegram_Script);
-  if (window.Telegram.WebApp.initDataUnsafe.user) {
+  if (window.Telegram.WebApp.initDataUnsafe.user?.first_name) {
     nombre.value = window.Telegram.WebApp.initDataUnsafe.user?.first_name
   }
-  //Comprobar que los servidores funcionen
-  
+
+  status.value = await getStatus()
 
 })
+
+const getStatus = async ():Promise<boolean> => {
+  let salida = false
+  const configuracion = {
+
+    method: 'GET',
+    url: `${import.meta.env.VITE_APP_Web_IP}/status`
+  }
+
+try {
+  let respuesta  = await axios(configuracion)
+  if (respuesta.status === 200) salida = true 
+  
+} catch (error) {
+  console.log(error)
+}
+return salida
+
+}
 
 
 
