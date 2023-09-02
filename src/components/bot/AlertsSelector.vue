09e202b341428c0a3770324572a9b21549808b35
eduardocoello16 <eduardocoello16@gmail.com>
2023-09-02 10:26:27 +0100

    selectior

--- src/components/bot/AlertsSelector.vue
+++ src/components/bot/AlertsSelector.vue
@@ -4,6 +4,7 @@ import obras from '@/assets/tiposalertas/obras.svg';
 import retenciones from '@/assets/tiposalertas/retenciones.svg';
 import viacortada from '@/assets/tiposalertas/viacortada.svg'
 import otro from '@/assets/tiposalertas/otro.svg';
+import { RouterLink } from 'vue-router';
 const selectores = [
   {
     tipo: 'accidente',
@@ -29,28 +30,44 @@ const selectores = [
 </script>
 <template>
   <main>
-
-  <div class="tipoalerta" v-for="selector in selectores" :key="selector.tipo" :style="{ 'background-image': 'url(' + selector.image + ')' }"></div>
+    <h2>Selecciona un tipo de alerta</h2>
+    <section id="selector">
+      <RouterLink v-for="selector in selectores" :key="selector.tipo" to="{path: '/bot/' + selector.tipo}">
+        <div class="tipoalerta" :style="{ 'background-image': 'url(' + selector.image + ')' }"></div>
+      </RouterLink>
+    </section>
   </main>
 </template>
     
-
-    
 <style scoped >
-  .tipoalerta{
-
-width: 100px;
-height: 100px;
-background-repeat: no-repeat;
-background-size: contain;
-background-position: center center ;
-border-radius: 1em;
-cursor: pointer;
-margin: 0.4em;
-&:hover{
-  transform: scale(1.2);
+main {
+  display: flex;
+  flex-wrap: wrap;
 
 }
+
+#selector {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 2rem;
+}
+
+
+.tipoalerta {
+
+  width: 100px;
+  height: 100px;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center center;
+  border-radius: 1em;
+  cursor: pointer;
+  margin: 0.4em;
+
+  &:hover {
+    transform: scale(1.2);
+
+  }
 
 }
 </style>
