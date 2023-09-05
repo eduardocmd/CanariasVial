f0f63910f01ad0f00e696699bf344330f6c63815
eduardocoello16 <eduardocoello16@gmail.com>
2023-09-05 11:41:50 +0100

    dasdas

--- src/views/bot/IndexBotView.vue
+++ src/views/bot/IndexBotView.vue
@@ -2,7 +2,8 @@
   <main>
     <section v-if="status === 0">
       <!--Estado cargando-->
-      <img id="loadder" src="../../assets/loading.svg" alt="">
+      <span class="loader"></span>
+     
     </section>
     <section v-if="status === 200">
       <!--Estado funciona el server-->
@@ -91,16 +92,7 @@ onMounted(async () => {
 #dont-work img{
   height: 200px;
 }
-#loadder {
-  height: 100px;
-   position: absolute;
-   margin-left: auto;
-   margin-right: auto;
-   left: 0;
-   right: 0;
-   bottom: 50%;
-   text-align: center;
- }
+
 
  #version {
    position: absolute;
@@ -115,4 +107,81 @@ onMounted(async () => {
    background-color: var(--color-background-soft);
 
  }
+ .loader {
+  width: 96px;
+  height: 48px;
+  display: inline-block;
+  position: relative;
+  background:var(--color-hint);
+  border-radius: 48px 48px 0 0;
+  box-sizing: border-box;
+  overflow: hidden;
+  position: absolute;
+   margin-left: auto;
+   margin-right: auto;
+   left: 0;
+   right: 0;
+   bottom: 50%;
+   text-align: center;
+}
+.loader::after {
+  content: '';  
+  box-sizing: border-box;
+  position: absolute;
+  width: 24px;
+  height: 12px;
+  border-radius: 24px 24px 0 0;
+  background: var(--color-text);
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 0;
+}
+.loader::before {
+  content: '';  
+  position: absolute;
+  width: 4px;
+  height: 32px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  bottom: 0;
+  background:  var(--color-text);
+  transform-origin: 50% 100%;
+  box-sizing: border-box;
+  animation: animloader 2s linear infinite alternate;
+}
+
+@keyframes animloader {
+  0% {
+    transform: rotate(-70deg);
+  }
+  10% {
+    transform: rotate(-40deg);
+  }
+  20%, 45%, 35% {
+    transform: rotate(-10deg);
+  }
+  40%, 30% {
+    transform: rotate(-30deg);
+  }
+  50%, 60% {
+    transform: rotate(20deg);
+  }
+  55%, 65%, 75% {
+    transform: rotate(40deg);
+  }
+  70% {
+    transform: rotate(45deg);
+  }
+  85%, 90% {
+    transform: rotate(50deg);
+  }
+  95% {
+    transform: rotate(75deg);
+  }
+  100%, 93% {
+    transform: rotate(70deg);
+  }
+}
+
 </style>
