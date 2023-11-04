7d1620c46acfd2792ac92ee426cea8194e2c8bb6
eduardocoello16 <eduardocoello16@gmail.com>
2023-11-04 19:03:19 +0000

    añadidos

--- src/App.vue
+++ src/App.vue
@@ -58,11 +58,22 @@ onMounted(async () => {
 aside{
   position: fixed;
   width: 80%;
+ 
   height: var(--menu-size);
   bottom: 0;
-  background-color: rebeccapurple;
+  background-color:  var(--color-background-soft);
   left: 50%; /* Coloca el borde izquierdo del div a la mitad del contenedor */
   transform: translate(-50%, -50%); /* Centra el contenido */ 
+}
+.layout-content{
+
+border-top: 1px solid var(--surface-border);
+overflow: auto;
+
+padding: 2rem;
+background-color: var(--color-background);
+border-top-left-radius: 20px;
+box-shadow: inset 0 3px 4px #0000001a;
 }
   
 @media screen and (min-width: 768px) {
@@ -73,7 +84,7 @@ aside{
   overflow: auto;
   height: calc(100vh -  var(--menu-size));
   padding: 2rem;
-  background-color: rebeccapurple;
+  background-color: var(--color-background);
   border-top-left-radius: 20px;
   box-shadow: inset 0 3px 4px #0000001a;
 }
@@ -81,7 +92,7 @@ aside{
 
   padding-left:  var(--menu-size);
   padding-top:  var(--menu-size);
-  background-color: rgb(95, 65, 65);
+  background-color:  var(--color-background-soft);
   
 }
 
@@ -90,6 +101,7 @@ aside{
     display: flex;
     align-items: end;
     background-color: transparent;
+  
     top: 0;
     left: 0;
     transform: translateX(0);
--- src/components/HeadderApp.vue
+++ src/components/HeadderApp.vue
@@ -1,28 +1,51 @@
 <template>
-   <header> <h2>headder</h2></header>
+    <header>
+        <div id="logo" :style="{ 'background-image': 'url(./canariasvial.svg)' }"></div>
+
+        <h1>Canarias Vial</h1>
+    </header>
 </template>
 <style scoped>
-
-header{
-
-overflow: hidden;
-background-color: rebeccapurple;
-
-width: 100%;
+header {
+    background-color: var(--color-background-soft);
+    overflow: hidden;
+    display: flex;
+    text-align: center;
+    align-items: center;
+    padding: 0.5rem;
+    width: 100%;
+  
 }
 
+
+#logo {
+    height: 60px;
+    width: 60px;
+    background-size: 110%;
+    background-position: center center;
+    background-color: var(--color-background);
+    border-radius: 50%;
+
+    background-repeat: no-repeat;
+
+}
 
 @media screen and (min-width: 768px) {
+    #logo {
+        width: 100px;
+        height: 100px;
+    }
 
-header{
-    position: fixed;
-    background-color: transparent;
-    top: 0;
-    height: var(--menu-size);
-    width: 100%;
-    left: 0;
-    z-index: 999;
-}
+    header {
+        position: fixed;
+        background-color: transparent;
+
+        top: 0;
+        max-height: var(--menu-size);
+        width: 100%;
+        left: 0;
+        z-index: 1;
+    }
 
 }
 </style>
