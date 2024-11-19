b90db45be9bdf1c577a6b1a0fd2a3e64adfb04d4
Eduardo Casa <eduardo.coello@evm.net>
2024-11-19 16:13:02 +0000

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,39 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import { useRoute } from "vue-router";
-import HeadderApp from "@/components/HeadderApp.vue"
-import FooterWeb from "@/components/FooterWeb.vue"
+import HeadderApp from "@/components/HeadderApp.vue";
+import FooterWeb from "@/components/FooterWeb.vue";
 import AsideMenu from './components/AsideMenu.vue';
-
 
 // Obtén la información de la ruta actual
 const route = useRoute();
 
-const isInBot = computed(() => {
-  return route.meta.hideHeader === true;
-});
-
+const isInBot = computed(() => route.meta.hideHeader === true);
 </script>
 
 <template>
-  <HeadderApp v-if="!isInBot" />
-  <AsideMenu v-if="!isInBot"/>
+  <!-- Layout normal -->
+  <div v-if="!isInBot">
+    <HeadderApp />
+    <AsideMenu />
+    <main class="layout-wrapper">
+      <div class="layout-content">
+        <RouterView />
+      </div>
+      <FooterWeb />
+    </main>
+  </div>
 
-
-  <main class="layout-wrapper" v-if="!isInBot">
-    <div class="layout-content" v-if="!isInBot">
-
-      <RouterView v-if="!isInBot" />
-<FooterWeb v-if="!isInBot"/>
-    </div>
-
-  </main>
-
- 
-  <!--Vista para el bot-->
-  <RouterView v-if="isInBot" />
+  <!-- Vista para el bot -->
+  <RouterView v-else />
 </template>
 
 <style scoped>
 .layout-content {
   border-radius: 2rem;
   background-color: rgb(156, 156, 156);
-}
-
-
-
-.layout-content {
-
   border-top: 1px solid var(--surface-border);
   overflow: auto;
-
   padding: 1.5rem;
   background-color: var(--color-background);
   border-top-left-radius: 20px;
@@ -54,30 +41,14 @@ const isInBot = computed(() => {
 }
 
 @media screen and (min-width: 768px) {
-
   .layout-content {
-
-    border-top: 1px solid var(--surface-border);
-    overflow: auto;
     height: calc(100vh - var(--menu-size));
     padding: 1rem;
-    background-color: var(--color-background);
-    border-top-left-radius: 20px;
-    box-shadow: inset 0 3px 4px #0000001a;
   }
-
   .layout-wrapper {
-
     padding-left: var(--menu-size);
     padding-top: var(--menu-size);
     background-color: var(--color-background-soft);
-
   }
-
-
-
-
-
-
 }
 </style>
