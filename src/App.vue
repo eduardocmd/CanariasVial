ce57cd28ffdd45b3021ad6104c0355418c749cf4
eduardocoello16 <eduardocoello16@gmail.com>
2025-05-12 18:45:02 +0100

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from "vue-router";
+import HeadderApp from "@/components/HeadderApp.vue";
+import FooterWeb from "@/components/FooterWeb.vue";
+import AsideMenu from './components/AsideMenu.vue';
+
+// Obtén la información de la ruta actual
+const route = useRoute();
+
+const isInBot = computed(() => route.meta.hideHeader === true);
+</script>
+
 <template>
   <!-- Layout normal -->
   <div v-if="!isInBot">
@@ -14,22 +27,6 @@
   <!-- Vista para el bot -->
   <RouterView v-else />
 </template>
-<script setup lang="ts">
-import { computed } from 'vue';
-import { useRoute } from "vue-router";
-import HeadderApp from "@/components/HeadderApp.vue"
-import FooterWeb from "@/components/FooterWeb.vue"
-import AsideMenu from './components/AsideMenu.vue';
-
-
-// Obtén la información de la ruta actual
-const route = useRoute();
-
-const isInBot = computed(() => {
-  return route.meta.hideHeader === true;
-});
-
-</script>
 
 <style scoped>
 .layout-content {
